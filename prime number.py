6f9303ae7991287652f5838cee4fbaa850c0caed
priyankaudhani <priyankaudhani3@gmail.com>
2026-03-12 15:29:57 +0530

    Add files via upload

---
+++ prime number.py
@@ -0,0 +1,10 @@
+# prime number the number taken by user
+num = int(input("Enter a number: "))
+# prime number is a number that is only divisible by 1 and itself
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
